c83f2d518416cce48ed685c935d85b3932d39e8a
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2024-10-02 10:02:59 +0530

    any

--- jsmethod.js
+++ jsmethod.js
@@ -12,7 +12,6 @@
 // console.log(Array.isArray([1,2,3,4])); //true
 // console.log(Array.isArray("sagar")); false
 
-
 // 3-Array.of //////////////////////////////////////////////////
 
 // it convert object to Array
@@ -22,7 +21,6 @@
 
 // let res="sagar"
 // console.log(Array.of(res));
-
 
 // console.log(Array.of());
 
@@ -46,17 +44,17 @@
 
 // 6-every method
 
-// this method return boolen value if all element satisfy the condition then return true 
+// this method return boolen value if all element satisfy the condition then return true
 // else one element not satisfy contion then return false
 
 // let arr1=[1,2,3,4,5,6,7,8,9,10]
 // let res=arr1.every(fin=> fin<10)
 // console.log(res);
 
-// 7-fill method 
-// fill method replace Element in the array, this method have thiree parameter first is 
-// replace element ,second is staring index and third is end index 
- 
+// 7-fill method
+// fill method replace Element in the array, this method have thiree parameter first is
+// replace element ,second is staring index and third is end index
+
 // let arr1=[1,2,3,4,5,6,7,8,9]
 // let res=arr1.fill(9,3,9)
 // console.log(res);
@@ -76,7 +74,7 @@
 // })
 // console.log(res);
 
-// 9-map 
+// 9-map
 // this method  itetare the each element of tha array
 // and return a new Array
 
@@ -102,7 +100,7 @@
 // let res=arr1.findIndex(cv=>cv>6)
 // console.log(res);
 
-// 12-flat method 
+// 12-flat method
 
 // flat method convert nested array to single array
 
@@ -113,13 +111,13 @@
 
 // 13-foreach method
 
-// foreach method provied a function each element of array 
+// foreach method provied a function each element of array
 // foreach method return undefine
 
 // let arr1=[1,2,3,4,5,6,7,8,9,10]
 // let cv=arr1.forEach((res,i)=>{
 //     console.log(`${i+1}:-${res} `);
-    
+
 // })
 
 // 14-includes
@@ -129,8 +127,8 @@
 // let fin=arr1.includes(2)
 // console.log(fin);
 
-// 15-indexOf 
-// indexOf method check element presnt or not if presnt then return index of element 
+// 15-indexOf
+// indexOf method check element presnt or not if presnt then return index of element
 // elese -1
 
 // const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
@@ -154,7 +152,7 @@
 // let res=beasts.keys()
 // for(let id of res){
 //     console.log(id);
-    
+
 // }
 
 // 18-lastIndexOf
@@ -203,8 +201,8 @@
 
 // 23-reduce
 
-// reduce method execute reduce function each element of Array and get output 
-// single value in accumulator.reduce method have two parameter accumulator and 
+// reduce method execute reduce function each element of Array and get output
+// single value in accumulator.reduce method have two parameter accumulator and
 // previous value
 // let arr1=[1,2,3,4,5,56,6,7,9]
 // let red=arr1.reduce((acc,pre)=>{
@@ -222,7 +220,7 @@
 
 // 25-slice
 
-// slice method is used to remove the Element from Array.slice method have two 
+// slice method is used to remove the Element from Array.slice method have two
 // parameter starting index and end index
 
 // let arr1=[1,2,3,4,5,56,6,7,9]
@@ -233,8 +231,8 @@
 // 26-splice
 // let arr1=[1,2,3,4,5,56,6,7,9]
 // splice method are used to add and remove the element of Array
-// splice method have n number of parameter first parameter is start index second 
-// index is how many index is deleted apart from two parameter other element is  add 
+// splice method have n number of parameter first parameter is start index second
+// index is how many index is deleted apart from two parameter other element is  add
 // in index
 
 // let res=arr1.splice(2,1,99,101,102)
@@ -265,11 +263,10 @@
 // let fin=months.toString()
 // console.log(fin)
 
-
 // ////////OBJECT METHOD //////////////////////////////////////////////
 
 // 1-charAt method
-// charAt method access to each element of string 
+// charAt method access to each element of string
 // let str="sagar"
 // let res=str.charAt(0)
 // console.log(res);
@@ -293,13 +290,13 @@
 
 // 5-replace
 
-// replace method replace element in the string 
+// replace method replace element in the string
 // const paragraph = "I think Ruth's dog is cuter than your dog!";
 
 // console.log(paragraph.replace("Ruth's","my"));
 
-// 6-/replaceAll 
-// replaceAll replace the multiple string 
+// 6-/replaceAll
+// replaceAll replace the multiple string
 // const paragraph = "I think Ruth's dog is cuter than your dog!";
 // let res=paragraph.replaceAll("dog","monkey")
 // console.log(res);
@@ -344,73 +341,107 @@
 // let res=greeting.trimStart()
 // console.log(res);
 
-
 /////////////////////////////////OBJECT/////////////////////////////////////////////
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// 1-Object.assign()
+// Object.assign() method is used to merge the multiple array and create a shallow copy of the object
+
+// let obj1 = {
+//   name: "sagar",
+//   roll: 21,
+//   city: "bhubaneswar",
+// };
+// let obj2 = {
+//   name: "sipu",
+//   roll: 1601201039,
+//   city: "biso",
+//   bloodGroup:"o+"
+// };
+
+// let res = Object.assign({}, obj1); // here we have create a shallow copy of obj1,
+//becase object copy of memory location
+// res.name = "any";
+// console.log(res);
+// console.log(obj1);
+
+// let mer=Object.assign(obj1,obj2)
+// console.log(mer);
+
+// 2-Object create
+// it create a Object
+// let res=Object.create({
+//     name:"sagar",
+//     roll:21,
+//     place:"bisoi"
+// })
+// delete res.name
+// console.log(res);
+
+// 3-Object.keys
+// Object.keys method return object proparty name with array
+
+// const object1 = {
+//     a: 'somestring',
+//     b: 42,
+//   };
+//   let res=Object.keys(object1)
+//   console.log(res);
+
+//   4-Object.values
+// Object.value return object propaty name with Array
+// let obj1={
+//     name:"sagar",
+//     roll:21,
+//     surename:"mohapatra"
+// }
+
+// let res=Object.values(obj1)
+// console.log(res);
+
+
+// 5-Object.freeze
+
+// object freeze method freeze object so we unable change object proparty and delete the proparty of object
+// const obj = {
+//     roll: 42,
+//     name:"sagar"
+//   };
+
+//   let res=Object.freeze(obj)
+  
+//   console.log(res);
+  
+  
+// 6-Object.entries
+// object method convert multiple array of object proparty in array
+
+// const object1 = {
+//     a: 'somestring',
+//     b: 42,
+//   };
+//   let res=Object.entries(object1)
+//   console.log(res);
+  
+
+// 7-fromEntries
+// it convert (entries)array to object
+// const entries = new Map([
+//     ['foo', 'bar'],
+//     ['baz', 42],
+//   ]);
+
+//   let res=Object.fromEntries(entries)
+//   console.log(res);
+
+// 7-Object.hasOwn
+// Object.hasown propaty return boolen value they search object propaty name if do not find 
+// then return false else true
+// let obj1={
+//     name:"sagar",
+//     roll:21,
+//     surename:"mohapatra"
+// }
+// let res=Object.hasOwn(obj1,"any")
+// console.log(res);
 
 
